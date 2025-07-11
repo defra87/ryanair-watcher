df14568b9a90cd68760f702799de963c2996714c
defra87 <defra87@gmail.com>
2025-07-11 17:46:34 +0200

    config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+# config.py
+
+TRATTE = [
+    {
+        "origine": "TSF",
+        "destinazione": "DUB",
+        "dal": "2025-09-01",
+        "al": "2025-09-30",
+        "max_notti": 5,
+        "soglia": 40
+    }
+]
+
+ADULTI = 1
+BAMBINI = 0
+NEONATI = 0
+
+EMAIL_MITTENTE = "defra87@gmail.com"
+EMAIL_DESTINATARIO = "defra87@gmail.com"
+EMAIL_PASSWORD = "ulcneqsxjdahdwez"
